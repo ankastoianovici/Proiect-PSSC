903675fe2dfbc04c23c766cc2bacc04be03b74bc
CarinaSpin <61678323+CarinaSpin@users.noreply.github.com>
2022-12-16 18:49:28 +0200

    Add files via upload

---
+++ Proiect/Exemple/Example.Dto/Events/ProdusePublishedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Example.Dto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Dto.Events
+{
+    public record ProdusePublishedEvent
+    {
+        public List<ListaProduseDto> Produse { get; init; }
+    }
+}
---
+++ Proiect/Exemple/Example.Dto/Models/ListaProduseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Dto.Models
+{
+   
+    public record ListaProduseDto
+    {
+        public string IdOm { get; init; }
+        public string IdComanda { get; init; }
+        public decimal Cantitate { get; init; }
+        public decimal Pretbuc { get; init; }
+        public decimal PretFinal { get; init; }
+        public string Adresa { get; init; }
+    }
+}
