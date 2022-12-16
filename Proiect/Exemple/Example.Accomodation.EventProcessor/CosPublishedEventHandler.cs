44e6c896e2fb25b778d5232dfde62cacb656e9e5
CarinaSpin <61678323+CarinaSpin@users.noreply.github.com>
2022-12-16 19:19:08 +0200

    Add files via upload

---
+++ Proiect/Exemple/Example.Accomodation.EventProcessor/CosPublishedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Exemple.Domain.Models.CosPublishedEvent;
+using Example.Dto.Events;
+using Example.Events;
+using Example.Events.Models;
+
+namespace Example.Accomodation.EventProcessor
+{
+    internal class ProdusePublishedEventHandler : AbstractEventHandler<ProdusePublishedEvent>
+    {
+        public override string[] EventTypes => new string[] { typeof(ProdusePublishedEvent).Name };
+
+        protected override Task<EventProcessingResult> OnHandleAsync(ProdusePublishedEvent eventData)
+        {
+            Console.WriteLine(eventData.ToString());
+            return Task.FromResult(EventProcessingResult.Completed);
+        }
+    }
+}
---
+++ Proiect/Exemple/Example.Accomodation.EventProcessor/Worker.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Example.Events;
+
+namespace Example.Accomodation.EventProcessor
+{
+    internal class Worker : IHostedService
+    {
+        private readonly IEventListener eventListener;
+
+        public Worker(IEventListener eventListener)
+        {
+            this.eventListener = eventListener;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Worker started...");
+            return eventListener.StartAsync("cos", "accomodation", cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Worker stoped!");
+            return eventListener.StopAsync(cancellationToken);
+        }
+    }
+}
