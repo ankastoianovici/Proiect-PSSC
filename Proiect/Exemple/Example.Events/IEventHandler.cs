1fb4a2ba0a20b72983a1eba9e2d40056e5c0fdb6
CarinaSpin <61678323+CarinaSpin@users.noreply.github.com>
2022-12-16 18:50:54 +0200

    Add files via upload

---
+++ Proiect/Exemple/Example.Events/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudNative.CloudEvents;
+using Example.Events.Models;
+
+namespace Example.Events
+{
+    public interface IEventHandler
+    {
+        string[] EventTypes { get; }
+
+        Task<EventProcessingResult> HandleAsync(CloudEvent cloudEvent);
+    }
+}
---
+++ Proiect/Exemple/Example.Events/IEventSender.cs
@@ -0,0 +1,17 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Events
+{
+    public interface IEventSender
+    {
+        public static void Main(string[] args)
+        {
+        }
+        TryAsync<Unit> SendAsync<T>(string topicName, T @event);
+    }
+}
