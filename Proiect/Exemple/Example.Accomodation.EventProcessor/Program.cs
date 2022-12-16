26dcbb6cdd6eeb5f1a58e95d29e77c023623bfad
Anka-Snezana Stoianovici <61665120+ankastoianovici@users.noreply.github.com>
2022-12-16 18:57:43 +0200

    Create Program.cs

---
+++ Proiect/Exemple/Example.Accomodation.EventProcessor/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using Example.Events.ServiceBus;
+using Example.Events;
+
+namespace Example.Accomodation.EventProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddAzureClients(builder =>
+                {
+                    builder.AddServiceBusClient(hostContext.Configuration.GetConnectionString("ServiceBus"));
+                });
+
+                services.AddSingleton<IEventListener, ServiceBusTopicEventListener>();
+                services.AddSingleton<IEventHandler, ProdusePublishedEventHandler>();
+
+                services.AddHostedService<Worker>();
+            });
+        }
+    }
+}
+
